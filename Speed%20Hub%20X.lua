93e44c229a1076c4b7aa381ac590c6aa74c749b9
AhmadY99 <love1girls10@gmail.com>
2025-07-26 19:58:21 +0700

    Update Speed%20Hub%20X.lua

--- Speed%20Hub%20X.lua
+++ Speed%20Hub%20X.lua
@@ -1,2 +1,8 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/AhmadV99/Speed-Hub-X/main/Speed%20Hub%20X.lua", true))()
 loadstring(game:HttpGet("https://api.rubis.app/v2/scrap/XYBn1fK5fcalCz40/raw", true))()
+local Games = loadstring(game:HttpGet("https://raw.githubusercontent.com/AhmadV99/Speed-Hub-X/main/GameList.lua"))()
+
+local URL = Games[game.PlaceId]
+
+if URL then
+  loadstring(game:HttpGet(URL))()
+end
